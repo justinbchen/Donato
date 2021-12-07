57e0840a0658f2e5995e60577742ef62ff4ccf42
Justin Chen <justinbrianchen@yahoo.com>
2021-12-07 17:52:49 -0500

    enter location

--- main.js
+++ main.js
@@ -4,8 +4,27 @@
     });
 });
 
-[...document.getElementById('btn-pickup')].forEach((button) => {
-    button.addEventListener('click', () => {
-        
-    });
+document.getElementById('btn-pickup').addEventListener('click', () => {
+    document.getElementById('pickup').classList.remove('d-none');
+    document.getElementById('dropoff').classList.add('d-none');
+
+    document.getElementById('btn-pickup').classList.add('active');
+    document.getElementById('btn-dropoff').classList.remove('active');
+});
+
+document.getElementById('btn-dropoff').addEventListener('click', () => {
+    document.getElementById('dropoff').classList.remove('d-none');
+    document.getElementById('pickup').classList.add('d-none');
+
+    document.getElementById('btn-dropoff').classList.add('active');
+    document.getElementById('btn-pickup').classList.remove('active');
+});
+
+document.getElementById('addresses').addEventListener('change', () => {
+    if (document.getElementById('addresses').value == 'new-address') {
+        document.getElementById('enter-address').classList.remove('d-none');
+    }
+    else {
+        document.getElementById('enter-address').classList.add('d-none');
+    }
 });
