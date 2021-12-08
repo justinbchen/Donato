64ca609736c2d08fa3b742ec1052294333e51cb1
Tony Li <505012935@qq.com>
2021-12-08 11:12:36 -0500

    change

--- main.js
+++ main.js
@@ -23,8 +23,17 @@ document.getElementById('btn-dropoff').addEventListener('click', () => {
 document.getElementById('addresses').addEventListener('change', () => {
     if (document.getElementById('addresses').value == 'new-address') {
         document.getElementById('enter-address').classList.remove('d-none');
-    }
-    else {
+    } else {
         document.getElementById('enter-address').classList.add('d-none');
     }
 });
+$(".common-line").click(function () {
+    var index = $(this).index();
+    $("#four-left .common-line").removeClass("active");
+    $(this).addClass("active");
+    $("#four-left .left-icon").removeClass("active-icon").addClass("common-icon");
+    $(this).children(".left-icon").removeClass("common-icon").addClass("active-icon");
+    $("#four-right .detail-card").removeClass("card-display");
+    $("#four-right .detail-card").eq(index).addClass("card-display");
+
+})
